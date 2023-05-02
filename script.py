1214e31249e9ae0b3f7338fdf41c069b2ffe2911
korewashiroi <angelocarasig15@gmail.com>
2023-05-03 00:35:38 +1000

    Create script.py

---
+++ script.py
@@ -0,0 +1,16 @@
+import os
+print("Credits: https://github.com/angelocarasig")
+directory = input("Directory Path: ")
+input_string = input("Input String: ")
+replacement_string = input("Replacement String: ")
+
+try:
+    for filename in os.listdir(directory):
+        if input_string in filename:
+            new_filename = filename.replace(input_string, replacement_string)
+            os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+    print("Strings replaced successfully!")
+except FileNotFoundError:
+    print("File Location not found!")
+except Exception as e:
+    print(f"Unknown Error: ${e}")
